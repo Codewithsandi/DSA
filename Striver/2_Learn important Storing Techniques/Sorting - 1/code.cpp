6a76082c02bf7e0f83579110d3d2d8d15e8034c6
Codewithsandi <codersandipan.com>
2024-07-17 01:01:27 +0530

    urgent

--- Striver/2_Learn important Storing Techniques/Sorting - 1/code.cpp
+++ Striver/2_Learn important Storing Techniques/Sorting - 1/code.cpp
@@ -60,10 +60,37 @@ void Insertion_sort(int arr[], int n)
         int j = i;
         while (j > 0 && arr[j - 1] > arr[j])
         {
-            int temp = arr[j-1];
-            arr[j-1] = arr[j];
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
             arr[j] = temp;
             j--;
+        }
+    }
+}
+
+void sort1(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+void sort2(int arr[], int n)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
         }
     }
 }
@@ -89,7 +116,7 @@ int main()
     }
     cout << endl;
 
-    Insertion_sort(arr, n);
+    sort2(arr, n);
 
     cout << endl;
     cout << "before shorting: ";
